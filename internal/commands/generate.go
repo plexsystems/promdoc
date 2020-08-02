0efe8f60fd08952b679c537119fb867971a02ca3
Roberto L. Taborda <robert@zelinzky.dev>
2020-08-02 14:00:01 -0500

    Change output-dir argument to --out flag
    --out receives a file-path or file-name
    If not set by the user the flag has a default value of alerts.md
    If the flag is set to a directory path the ouput will be a file named alerts.md in that path
    paths to a directory should finish with "/"
    If the file is set to a file-name with no path current working dir will be used

--- internal/commands/generate.go
+++ internal/commands/generate.go
@@ -5,9 +5,9 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
-	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/plexsystems/promdoc/internal/rendering"
 )
@@ -15,12 +15,18 @@ import (
 // NewGenerateCommand creates a new generate command
 func NewGenerateCommand() *cobra.Command {
 	cmd := cobra.Command{
-		Use:   "generate <output-dir>",
+		Use:   "generate",
 		Short: "Generate documentation from a given folder",
-		Args:  cobra.ExactArgs(1),
+
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if err := viper.BindPFlag("out", cmd.Flags().Lookup("out")); err != nil {
+				return fmt.Errorf("bind out flag: %w", err)
+			}
+			return nil
+		},
 
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if err := runGenerateCommand(args[0]); err != nil {
+			if err := runGenerateCommand(); err != nil {
 				return fmt.Errorf("generate: %w", err)
 			}
 
@@ -28,27 +34,33 @@ func NewGenerateCommand() *cobra.Command {
 		},
 	}
 
+	cmd.Flags().StringP("out", "o", "alerts.md",
+		"file name or path for the output-file")
+
 	return &cmd
 }
 
-func runGenerateCommand(outputFile string) error {
+func runGenerateCommand() error {
+	out := viper.GetString("out")
+	outputDir, outputFile := path.Split(out)
 	workingDir, err := os.Getwd()
 	if err != nil {
 		return fmt.Errorf("get working dir: %w", err)
 	}
-
-	fileTokens := strings.Split(outputFile, ".")
-	if len(fileTokens) == 0 {
-		return fmt.Errorf("get file extension: %w", err)
+	if outputDir == "" {
+		outputDir = workingDir
+	}
+	if outputFile == "" {
+		outputFile = "alerts.md"
 	}
 
-	fileExtension := fileTokens[len(fileTokens)-1]
+	fileExtension := path.Ext(outputFile)
 	output, err := rendering.Render(workingDir, fileExtension)
 	if err != nil {
 		return fmt.Errorf("rendering: %w", err)
 	}
 
-	outputPath := path.Join(workingDir, outputFile)
+	outputPath := path.Join(outputDir, outputFile)
 	err = ioutil.WriteFile(outputPath, []byte(output), os.ModePerm)
 	if err != nil {
 		return fmt.Errorf("write document: %w", err)
